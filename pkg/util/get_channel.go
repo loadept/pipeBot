c0430ccd3d9464066c29b81e00bae6d4b8472eb7
jsusmachaca <falcorgd@gmail.com>
2025-01-16 21:33:55 -0500

    feat: add function to get channel name

--- pkg/util/get_channel.go
+++ pkg/util/get_channel.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -16,4 +17,25 @@ func GetVoiceChannel(s *discordgo.Session, userID string) (string, error) {
 		}
 	}
 	return "", fmt.Errorf("User not in voice channel")
+}
+
+func GetGuildChannel(s *discordgo.Session, guildID string, matchString string) string {
+	channels, err := s.GuildChannels(guildID)
+	if err != nil {
+		return ""
+	}
+
+	for _, ch := range channels {
+		match, err := regexp.MatchString(matchString, ch.Name)
+		if err != nil {
+			return ""
+		}
+		if !match {
+			continue
+		}
+
+		return ch.ID
+	}
+
+	return ""
 }
